d7b4f0c15e90d53c30aea8a30aa1735b151c471f
Jakub Janczak <jakub.janczak@schibsted.pl>
2017-12-09 19:12:34 +0100

    Some more or less fancy tests for quickcheck

--- 002-lists/FizzBuzz1/app/Main.hs
+++ 002-lists/FizzBuzz1/app/Main.hs
@@ -3,9 +3,5 @@ import System.Environment
 import System.Exit
 import Lib
 
-parseArg :: [String] -> IO Int
-parseArg [] = exitWith (ExitFailure 1)
-parseArg (x:xs) = return $ read x
-
 main :: IO ()
-main = getArgs >>= parseArg >>= (\n -> return $ fizzBuzz n) >>= print
+main = getArgs >>= return . read . head >>= (\n -> return $ fizzBuzz n) >>= print
--- 002-lists/FizzBuzz1/test/Spec.hs
+++ 002-lists/FizzBuzz1/test/Spec.hs
@@ -1,2 +1,17 @@
+import Test.QuickCheck
+import Lib
+
+countOccurrences :: String -> [String] -> Int
+countOccurrences s l = length $ filter ((==) s) l
+
+prop_aroundOneThirdAreFizz :: Int -> Bool
+prop_aroundOneThirdAreFizz n = (countOccurrences "Fizz" (fizzBuzz n)) == ((quot (n - 1) 3) - (quot (n - 1) 15))  || n <= 0
+
+prop_aroundOneFifthAreBuzz :: Int -> Bool
+prop_aroundOneFifthAreBuzz n = (countOccurrences "Buzz" (fizzBuzz n)) == ((quot (n - 1) 5) - (quot (n - 1) 15)) || n <= 0
+
+prop_aroundOneFifteenthAreFizzBuzz :: Int -> Bool
+prop_aroundOneFifteenthAreFizzBuzz n = (countOccurrences "Fizz Buzz" (fizzBuzz n)) == ((quot (n - 1) 15) + 1) || n <= 0
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck $ conjoin [prop_aroundOneThirdAreFizz, prop_aroundOneFifthAreBuzz, prop_aroundOneFifteenthAreFizzBuzz]
