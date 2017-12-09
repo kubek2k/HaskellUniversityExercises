ec9c6329c9871a6e6ae07649815bd4bbe922c137
Jakub Janczak <jakub.janczak@schibsted.pl>
2017-12-09 22:46:47 +0100

    Making testing ints non-negative

--- 002-lists/FizzBuzz1/test/Spec.hs
+++ 002-lists/FizzBuzz1/test/Spec.hs
@@ -4,14 +4,14 @@ import Lib
 countOccurrences :: String -> [String] -> Int
 countOccurrences s l = length $ filter ((==) s) l
 
-prop_aroundOneThirdAreFizz :: Int -> Bool
-prop_aroundOneThirdAreFizz n = (countOccurrences "Fizz" (fizzBuzz n)) == ((quot (n - 1) 3) - (quot (n - 1) 15))  || n <= 0
+prop_aroundOneThirdAreFizz :: NonNegative Int -> Property
+prop_aroundOneThirdAreFizz (NonNegative n) = n > 0 ==> (countOccurrences "Fizz" (fizzBuzz n)) == ((quot (n - 1) 3) - (quot (n - 1) 15)) 
 
-prop_aroundOneFifthAreBuzz :: Int -> Bool
-prop_aroundOneFifthAreBuzz n = (countOccurrences "Buzz" (fizzBuzz n)) == ((quot (n - 1) 5) - (quot (n - 1) 15)) || n <= 0
+prop_aroundOneFifthAreBuzz :: NonNegative Int -> Property
+prop_aroundOneFifthAreBuzz (NonNegative n) = n > 0 ==> (countOccurrences "Buzz" (fizzBuzz n)) == ((quot (n - 1) 5) - (quot (n - 1) 15))
 
-prop_aroundOneFifteenthAreFizzBuzz :: Int -> Bool
-prop_aroundOneFifteenthAreFizzBuzz n = (countOccurrences "Fizz Buzz" (fizzBuzz n)) == ((quot (n - 1) 15) + 1) || n <= 0
+prop_aroundOneFifteenthAreFizzBuzz :: NonNegative Int -> Property
+prop_aroundOneFifteenthAreFizzBuzz (NonNegative n) = n > 0 ==> (countOccurrences "Fizz Buzz" (fizzBuzz n)) == ((quot (n - 1) 15) + 1) 
 
 main :: IO ()
 main = quickCheck $ conjoin [prop_aroundOneThirdAreFizz, prop_aroundOneFifthAreBuzz, prop_aroundOneFifteenthAreFizzBuzz]
