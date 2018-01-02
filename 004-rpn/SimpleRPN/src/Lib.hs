35670d71e9358f9f065435c70bcfca9315c64938
Jakub Janczak <jakub.janczak@schibsted.pl>
2018-01-02 12:40:43 +0100

    Simple recursive RPN calculator

---
+++ 004-rpn/SimpleRPN/src/Lib.hs
@@ -0,0 +1,44 @@
+module Lib
+  ( parse
+  ) where
+
+data OperatorType
+  = Plus
+  | Minus
+  | Divide
+  | Multiply
+  deriving (Show)
+
+data Token
+  = Operator OperatorType
+  | Operand Float
+  deriving (Show)
+
+instance Read Token where
+  readsPrec _ "+" = [(Operator Plus, "")]
+  readsPrec _ "-" = [(Operator Minus, "")]
+  readsPrec _ "/" = [(Operator Divide, "")]
+  readsPrec _ "*" = [(Operator Multiply, "")]
+  readsPrec _ s = [(Operand (read s), "")]
+
+tokenize = words
+
+parse :: [String] -> [Token]
+parse = map read
+
+interpret :: [Token] -> Float
+interpret tokens = interpretInternal tokens []
+  where
+    interpretInternal (Operand f:rest) stack =
+      interpretInternal rest (f : stack)
+    interpretInternal (Operator o:rest) (stackHead:stackTailHead:stackTailTail) =
+      let operation =
+            case o of
+              Plus -> (+)
+              Minus -> (-)
+              Divide -> (/)
+              Multiply -> (*)
+      in interpretInternal
+           rest
+           (operation stackTailHead stackHead : stackTailTail)
+    interpretInternal [] (stackHead:_) = stackHead
