b11b2394ffa2354675b417053f0196aa6cb3a2b9
Jakub Janczak <jakub.janczak@schibsted.pl>
2018-01-03 21:38:51 +0100

    Styling changes

--- 003-shop/ShoppingCartPromotions/test/Spec.hs
+++ 003-shop/ShoppingCartPromotions/test/Spec.hs
@@ -1,10 +1,13 @@
-import Test.QuickCheck
 import Lib
+import Test.QuickCheck
 
 genFruits :: Gen [Fruit]
 genFruits = listOf $ elements [Apple, Orange]
 
-prop_IsCheaperAfterPromotion fruits = (length fruits) > 5 ==> (checkout fruits) > (checkoutPromotions fruits) where types = fruits :: [Fruit]
- 
+prop_IsCheaperAfterPromotion fruits =
+  length fruits > 5 ==> checkout fruits > checkoutPromotions fruits
+  where
+    types = fruits :: [Fruit]
+
 main :: IO ()
 main = quickCheck $ forAll genFruits prop_IsCheaperAfterPromotion
